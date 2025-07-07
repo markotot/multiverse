fix: normalizing the greyscale image moved from the agent to the environment

src/agent/actor_critic.py
@@ -26,12 +26,12 @@ class Agent(nn.Module):
         self.critic = layer_init(nn.Linear(512, 1), std=1)
 
     def get_value(self, observation):
-        hidden = self.network(observation / 255.0)
+        hidden = self.network(observation)
         value = self.critic(hidden)
         return value
 
     def get_action_and_value(self, observation, action=None):
-        hidden = self.network(observation / 255.0)
+        hidden = self.network(observation)
         logits = self.actor(hidden)
         value = self.critic(hidden)
         probs = Categorical(logits=logits)

src/trainer/runner.py
@@ -288,14 +288,14 @@ class Runner:
                 episode_lengths += 1
                 step += 1
 
-
-            create_video_from_images(
-                images=np.array(record_observations),  # Your numpy array of images
-                episode_returns=np.array(record_rewards),
-                output_path='./recordings/my_video.mp4',
-                fps=15,
-                scale_factor=6  # Makes 84x84 -> 504x504 for better visibility
-            )
+            if self.cfg.eval.save_video:
+                create_video_from_images(
+                    images=np.array(record_observations),  # Your numpy array of images
+                    episode_returns=np.array(record_rewards),
+                    output_path='./recordings/my_video.mp4',
+                    fps=15,
+                    scale_factor=6  # Makes 84x84 -> 504x504 for better visibility
+                )
 
             self.writer.add_scalar(f"eval/mean_episode_reward", np.mean(total_rewards), self.current_iteration)
             self.writer.add_scalar(f"eval/min_reward", np.min(total_rewards), self.current_iteration)