fix: changing normalization

src/world_models/iris_world_model/iris_env.py
@@ -142,7 +142,7 @@ class IrisEnv(nn.Module):
     def reset_from_initial_observation(self, observation: np.ndarray) -> list:
 
         obs = einops.rearrange(observation, 'b fs h w c -> (b fs) c h w')
-        obs = obs / 255.0 # Normalize to [0, 1]
+        obs = obs / 255.0 * 2 - 1  # Normalize to [-1, 1]
         obs = torch.from_numpy(obs).float().to(self.device)
         obs_tokens = self.tokenizer.encode(obs, should_preprocess=False).tokens  # (BL, K)
 
@@ -155,8 +155,29 @@ class IrisEnv(nn.Module):
         _ = self.refresh_keys_values_with_initial_obs_tokens(unstacked_obs_tokens)
 
         self.framestack_observation_tokens = einops.rearrange(obs_tokens, '(b fs) n -> b fs n', fs=fs)
-        decoded_observations = self.decode_obs_tokens(obs_tokens)
+        decoded_observations = self.decode_obs_tokens(obs_tokens, should_preprocess=False)
         decoded_observations = einops.rearrange(decoded_observations, '(b fs) c h w -> b fs c h w', fs=fs)
+
+        ###### plot the greyscale obs
+        # numpy_observations = obs[0]
+        # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+        # img1_np = obs.detach().cpu().numpy().squeeze()
+        # img2_np = decoded_observations.detach().cpu().numpy().squeeze()
+        #
+        # orig = np.transpose(img1_np[0], (1, 2, 0))
+        # redec = np.transpose(img2_np[0][0], (1, 2, 0))
+        # # Display the images
+        # ax1.imshow(orig)
+        # ax1.set_title('Image 1')
+        # ax1.axis('off')
+        #
+        # ax2.imshow(redec)
+        # ax2.set_title('Image 2')
+        # ax2.axis('off')
+        #
+        # plt.tight_layout()
+        # plt.show()
+        ###### plot the greyscale obs
 
         return decoded_observations
 
@@ -169,10 +190,10 @@ class IrisEnv(nn.Module):
         return outputs_wm.output_sequence  # (B, K, E)
 
     @torch.no_grad()
-    def decode_obs_tokens(self, obs_tokens) -> List[Image.Image]:
+    def decode_obs_tokens(self, obs_tokens, should_preprocess) -> List[Image.Image]:
         embedded_tokens = self.tokenizer.embedding(obs_tokens)     # (B, K, E)
         z = einops.rearrange(embedded_tokens, 'b (h w) e -> b e h w', h=int(np.sqrt(self.num_observations_tokens)))
-        rec = self.tokenizer.decode(z, should_postprocess=True)         # (B, C, H, W)
+        rec = self.tokenizer.decode(z, should_preprocess=should_preprocess)  # (B, C, H, W)
         return torch.clamp(rec, 0, 1)
 
     def reset(self, initial_observations) -> [np.ndarray, dict]:
@@ -240,7 +261,7 @@ class IrisEnv(nn.Module):
         self.framestack_observation_tokens[:, -1] = new_obs_tokens
 
         x = einops.rearrange(self.framestack_observation_tokens, 'b fs n -> (b fs) n')
-        x = self.decode_obs_tokens(x)
+        x = self.decode_obs_tokens(x, should_preprocess=True) # we want output to be normalized into greyscale [0,1]
         decoded_observations = einops.rearrange(x, '(b fs) c h w -> b fs c h w',
                                                 fs=4)
 

src/world_models/iris_world_model/networks/tokenizer/tokenizer.py
@@ -48,7 +48,7 @@ class Tokenizer(nn.Module):
 
         observation = dataset['rgb'][:, :, 0, :, :, :]# Take the first frame of the frame stack
         observations = einops.rearrange(observation, 'b t h w c -> (b t) c h w')
-        observations = observations / 255.0 * 2 - 1 # Normalize to [-1, 1]
+        observations = observations / 255.0 # Normalize to [-1, 1]
         z, z_quantized, reconstructions = self(observations, should_preprocess=False, should_postprocess=False)
 
         # Codebook loss. Notes:
@@ -64,6 +64,7 @@ class Tokenizer(nn.Module):
 
     def encode(self, x: torch.Tensor, should_preprocess: bool) -> TokenizerEncoderOutput:
         if should_preprocess:
+            assert False
             x = self.preprocess_input(x)
         shape = x.shape  # (..., C, H, W)
         x = x.view(-1, *shape[-3:])
@@ -83,14 +84,22 @@ class Tokenizer(nn.Module):
 
         return TokenizerEncoderOutput(z, z_q, tokens)
 
-    def decode(self, z_q: torch.Tensor, should_postprocess: bool = False) -> torch.Tensor:
+    def decode(self, z_q: torch.Tensor, should_preprocess) -> torch.Tensor:
+        """
+
+        :param z_q:
+        :param should_preprocess: normalizes from [-1, 1] into [0, 1]
+        :return:
+        """
         shape = z_q.shape  # (..., E, h, w)
         z_q = z_q.view(-1, *shape[-3:])
         z_q = self.post_quant_conv(z_q)
         rec = self.decoder(z_q)
         rec = rec.reshape(*shape[:-3], *rec.shape[1:])
-        if should_postprocess:
-            rec = self.postprocess_output(rec)
+
+        if should_preprocess:
+            rec = (rec + 1) / 2
+
         return rec
 
     @torch.no_grad()

src/world_models/iris_world_model/networks/world_model.py
@@ -100,7 +100,7 @@ class WorldModel(nn.Module):
         with torch.no_grad():
             observation = batch['rgb'][:, :, 0, :, :, :]  # Take the first frame of the frame stack
             observation = einops.rearrange(observation, 'b t h w c -> b t c h w')
-            observation = observation / 255.0 * 2 - 1  # Normalize to [-1, 1]
+            observation = observation / 255.0
             obs_tokens = tokenizer.encode(observation, should_preprocess=False).tokens  # (BL, K)
 
         act_tokens = einops.rearrange(batch['actions'], 'b l -> b l 1')